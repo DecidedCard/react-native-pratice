9a06a7ac209c52c3f2f2627f7b131989d9e84da0
jung hea jun <heajun35@gmail.com>
2025-09-08 14:58:44 +0900

    Feat: Expo Router와 _layout, Navigation

---
+++ app/modal.tsx
@@ -0,0 +1,16 @@
+import { useRouter } from "expo-router";
+import React from "react";
+import { Pressable, Text, View } from "react-native";
+
+export default function Modal() {
+  const router = useRouter();
+
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>modal</Text>
+      <Pressable onPress={() => router.back()}>
+        <Text>Close</Text>
+      </Pressable>
+    </View>
+  );
+}
