e02b037bc1994b9e8e0e0babbc13d01972820b83
jung hea jun <heajun35@gmail.com>
2025-09-10 19:36:32 +0900

    Feat: StyleSheet와 react-native-safe-area-context

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -1,49 +1,60 @@
 import { usePathname, useRouter } from "expo-router";
-import { Text, TouchableOpacity, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   const router = useRouter();
   const pathname = usePathname();
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <View>
-        <TouchableOpacity onPress={() => router.push("/")}>
-          <Text style={{ color: pathname === "/" ? "red" : "black" }}>
-            For you
-          </Text>
-        </TouchableOpacity>
-      </View>
-      <View>
-        <TouchableOpacity onPress={() => router.push("/following")}>
-          <Text style={{ color: pathname === "/" ? "black" : "red" }}>
-            Following
-          </Text>
-        </TouchableOpacity>
-      </View>
-      <View>
-        <TouchableOpacity onPress={() => router.push("/test/post/1")}>
-          <Text>게시글1</Text>
-        </TouchableOpacity>
-      </View>
+    <SafeAreaView style={style.container}>
+      <View style={style.tabContainer}>
+        <View style={style.tab}>
+          <TouchableOpacity onPress={() => router.push("/")}>
+            <Text style={{ color: pathname === "/" ? "red" : "black" }}>
+              For you
+            </Text>
+          </TouchableOpacity>
+        </View>
 
-      <View>
-        <TouchableOpacity onPress={() => router.push("/test/post/1")}>
-          <Text>게시글2</Text>
-        </TouchableOpacity>
-      </View>
+        <View style={style.tab}>
+          <TouchableOpacity onPress={() => router.push("/following")}>
+            <Text style={{ color: pathname === "/" ? "black" : "red" }}>
+              Following
+            </Text>
+          </TouchableOpacity>
+        </View>
 
-      <View>
-        <TouchableOpacity onPress={() => router.push("/test/post/1")}>
-          <Text>게시글3</Text>
-        </TouchableOpacity>
+        <View style={style.tab}>
+          <TouchableOpacity onPress={() => router.push("/test/post/1")}>
+            <Text>게시글1</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={style.tab}>
+          <TouchableOpacity onPress={() => router.push("/test/post/1")}>
+            <Text>게시글2</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={style.tab}>
+          <TouchableOpacity onPress={() => router.push("/test/post/1")}>
+            <Text>게시글3</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+    </SafeAreaView>
   );
 }
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  tabContainer: {
+    flexDirection: "row",
+  },
+  tab: {
+    flex: 1,
+  },
+});
