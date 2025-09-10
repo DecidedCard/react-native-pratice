6eb901f4b4bbcbfc87beea7d102b7e41018357b7
jung hea jun <heajun35@gmail.com>
2025-09-10 11:26:35 +0900

    Feat: push, replace, navigate의 차이

---
+++ app/login.tsx
@@ -0,0 +1,17 @@
+import { Redirect } from "expo-router";
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function Login() {
+  const isLoggedIn = false;
+
+  if (isLoggedIn) {
+    return <Redirect href={"/(tabs)"} />;
+  }
+
+  return (
+    <View>
+      <Text>Login</Text>
+    </View>
+  );
+}
