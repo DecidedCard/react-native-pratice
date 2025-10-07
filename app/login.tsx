fd43f00ac5e8c6248e3a5c9a424aa7447f38f465
jung hea jun <heajun35@gmail.com>
2025-10-07 19:49:33 +0900

    Feat: kakao 로그인 작성

--- app/login.tsx
+++ app/login.tsx
@@ -1,5 +1,7 @@
+import { initializeKakaoSDK } from "@react-native-kakao/core";
+import { login as kakaoLogin, me } from "@react-native-kakao/user";
 import { Redirect } from "expo-router";
-import React, { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { Pressable, StyleSheet, Text, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { AuthContext } from "./_layout";
@@ -9,29 +11,69 @@ export default function Login() {
   const { user, login } = useContext(AuthContext);
   const isLoggedIn = !!user;
 
+  useEffect(() => {
+    initializeKakaoSDK("11c6efe3d3f6f998dd715b0bc165ce8a");
+  }, []);
+
+  const onKakaoLogin = async () => {
+    const result = await kakaoLogin();
+    console.log(result);
+    const user = await me();
+    console.log(user);
+  };
+
   if (isLoggedIn) {
     return <Redirect href={"/(tabs)"} />;
   }
 
   return (
-    <View style={{ paddingTop: insets.top }}>
+    <View style={[styles.container, { paddingTop: insets.top }]}>
       <Text>Back</Text>
       <Pressable onPress={login} style={styles.loginButton}>
         <Text style={styles.loginButtonText}>Login</Text>
+      </Pressable>
+      <Pressable
+        onPress={onKakaoLogin}
+        style={[styles.loginButton, styles.kakaoLoginButton]}
+      >
+        <Text style={[styles.loginButtonText, styles.kakaoLoginButtonText]}>
+          KaKao Login
+        </Text>
+      </Pressable>
+      <Pressable
+        onPress={login}
+        style={[styles.loginButton, styles.appleLoginButton]}
+      >
+        <Text style={styles.loginButtonText}>Apple Login</Text>
       </Pressable>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
   loginButton: {
     backgroundColor: "blue",
-    padding: 10,
     borderRadius: 6,
-    width: 100,
+    width: 200,
+    height: 50,
     alignItems: "center",
+    justifyContent: "center",
+  },
+  kakaoLoginButton: {
+    backgroundColor: "yellow",
+  },
+  appleLoginButton: {
+    backgroundColor: "black",
   },
   loginButtonText: {
     color: "white",
+  },
+  kakaoLoginButtonText: {
+    color: "black",
   },
 });
